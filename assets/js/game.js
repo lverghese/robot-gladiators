eb09c3934cf913002c0b9587acbe58d1ec5e73d6
unknown <loganverghese@gmail.com>
2021-06-29 01:50:43 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,13 @@ var playerMoney = 10;
 //You can also log multiple values at once like this -> console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-for(var i = 0; i < enemyNames.length; i++) {
-   
-}
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
 
 
 
@@ -80,13 +82,6 @@ var fight = function(enemyName) {
     } else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-
-    //if player chooses to skip
-   
-    //if no (false), ask question again by running fight() again
-
-        
-
     }
 };
     
@@ -94,9 +89,28 @@ var fight = function(enemyName) {
   
     
 for(var i = 0; i < enemyNames.length; i++) {
+
+    if (playerHealth > 0) {
+
+    // let player know what round they are in, remember that the arrays start at 0 so it needs to have 1 added to it
+     window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
+
+    // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+
+    // reset enemyHealth before starting new fight 
     enemyHealth = 50;
+
+    //use debugger to pause script from running and check what's going on at that moment in the code
+    //debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+    } 
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
 
 // execute function
