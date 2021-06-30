5d45b504200e095919498ad3b421ba74cb4473ca
unknown <loganverghese@gmail.com>
2021-06-29 21:50:39 -0400

    math expressions

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,14 +40,14 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + " has decided to skip this fight. Goodbye!");
                 //subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney);
                 break;
             }
         }
    
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
-    enemyHealth = enemyHealth - playerAttack;
+    enemyHealth = Math.max(0, enemyHealth - playerAttack);
 
     // Log a resulting message to the console so that we know it worked
     console.log(
@@ -68,7 +68,7 @@ var fight = function(enemyName) {
     };
 
     //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-    playerHealth = playerHealth - enemyAttack;
+    playerHealth = Math.max(0, playerHealth - enemyAttack);
 
     // Log a resulting message to the console so that we know it worked
     console.log(
@@ -106,7 +106,7 @@ var startGame = function(){
             var pickedEnemyName = enemyNames[i];
 
             // reset enemyHealth before starting new fight 
-            enemyHealth = 50;
+            enemyHealth = Math.floor(Math.random() * 21) + 40;
 
              //use debugger to pause script from running and check what's going on at that moment in the code
             //debugger;
